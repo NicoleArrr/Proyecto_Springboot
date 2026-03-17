79eac7526dffbf9fa78286c276610184fd8cccce
NicoleArias <nicoledayanaariaspinzon@gmail.com>
2026-03-17 05:19:50 -0500

    feat :sparkles: working on configuration

--- LogiTrack/src/main/java/com/proyecto/LogiTrack/config/OpenAPIConfig.java
+++ LogiTrack/src/main/java/com/proyecto/LogiTrack/config/OpenAPIConfig.java
@@ -1,4 +1,19 @@
 package com.proyecto.LogiTrack.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Documentada para LogITrack S.A.")
+                        .version("1.0")
+                        .description("Esta API tiene como objetivo informar sobre la estructuración y el\n funcionamiento de las apis correspondientes a cada entidad a fin de ser consumidas \ny ejemplificadas mediante elementos de prueba a continuación"));
+    }
 }
